5aa92e3d7608a76d0efe27b84ff0620adeeabd32
Ste79ITA <nastri.s@gmail.com>
2023-10-23 16:37:33 +0200

    Vue experiments

--- js/app.js
+++ js/app.js
@@ -6,8 +6,19 @@ createApp({
       greetings: 'Hello Vue!',
       img: 'kotetsu-jeeg.jpg',
       path: '../img/',
+      input: '',
+      display: 'd-none',
     };
   },
+  methods: {
+    displayImg: function () {
+      console.log('TEST');
+      console.log(this.input);
+      console.log(this.display);
+      this.display = 'd-none'
+        ? (this.display = 'd-block')
+        : (this.display = 'd-none');
+      console.log(this.display);
+    },
+  },
 }).mount('#app');
-
-console.log(this.path, this.img);
